d0f861850da18af3459f02960f3b93078aa23617
R M Shahidul Islam <shahedbddev@gmail.com>
2024-11-08 22:46:03 +0600

    + Project code added

---
+++ Dotnet9CRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Dotnet9CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet9CRUD.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
